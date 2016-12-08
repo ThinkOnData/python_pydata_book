2df75a100ba8e8f3c2ee698669245a582db67913
-ABOER- <493970240@qq.com>
2016-12-08 15:25:16 +0800

    learn

--- char10/demo01.py
+++ char10/demo01.py
@@ -204,3 +204,90 @@ ts = pd.Series(np.arange(100), index=rng)
 # print ts.groupby(lambda x: x.month).mean()
 # print ts.groupby(lambda x: x.weekday).mean()
 
+# 升采样和插值
+frame = pd.DataFrame(np.random.randn(2, 4),
+                     index=pd.date_range("1/1/2000", periods=2, freq="W-WED"),
+                     columns=["Colorado", "Texas", "New York", "Ohio"])
+df_daily = frame.resample("D").ffill()
+# print df_daily
+
+# 通过时期进行重采样
+frame = pd.DataFrame(np.random.randn(24, 4),
+                     index=pd.period_range("1-2000", "12-2001", freq="M"),
+                     columns=["Colordo", "Texas", "New York", "Ohio"])
+# print frame
+# annual_frame = frame.resample("A-DEC").mean()
+# print annual_frame
+
+annual_frame = frame.resample("Q-DEC")
+# print annual_frame
+annual_frame1 = frame.resample("Q-DEC", convention="start")
+# print annual_frame1
+
+
+# 时间序列绘图
+close_px_all = pd.read_csv("E:\workspace_git\python_pydata_book\char09\stock_px.csv",
+                           parse_dates=True,
+                           index_col=0)
+close_px = close_px_all[["AAPL", "MSFT", "XOM"]]
+close_px = close_px.resample("B").ffill()
+# print close_px
+# close_px["AAPL"].plot()
+# close_px.ix["2009"].plot()
+
+# close_px["AAPL"].ix["01-2011":"03-2011"].plot()
+
+appl_q = close_px["AAPL"].resample("Q-DEC").ffill()
+# appl_q.ix["2009":].plot()
+
+# close_px.AAPL.plot()
+# pd.rolling_mean(close_px.AAPL,250).plot()
+
+
+appl_std250 = pd.Series.rolling(close_px.AAPL, 250, min_periods=10).std()
+# appl_std250.plot()
+
+# expandng_mean=lambda x: pd.Series.rolling(x,len(x),min_periods=1).mean()
+# pd.DataFrame.rolling(close_px, 60).mean().plot(logy=True)
+# pd.DataFrame.rolling(close_px, 60).mean().plot()
+# pd.DataFrame.rolling(close_px, 60).kurt().plot(logy=True)
+
+import matplotlib.pyplot as plt
+
+# fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True, figsize=(12, 7))
+# aapl_px = close_px.AAPL["2005":"2009"]
+# ma60 = pd.Series.rolling(aapl_px, 60, min_periods=50).mean()
+# ewma60 = pd.Series.ewm(aapl_px, span=60).mean()
+
+# aapl_px.plot(style="k-",ax=axes[0])
+# ma60.plot(style="k--",ax=axes[0])
+# aapl_px.plot(style="k-",ax=axes[1])
+# ewma60.plot(style="k--",ax=axes[1])
+# axes[0].set_title("Simple MA")
+# axes[1].set_title("Exponetially-weighted MA")
+
+
+# 二元移动窗口函数
+# spx_px = close_px_all["SPX"]
+# spx_rets = spx_px / spx_px.shift(1) - 1
+returns = close_px.pct_change()
+# corr = pd.rolling_corr(returns, spx_rets, 125, min_periods=100)
+# corr.plot()
+
+# 用户自定义移动窗口函数
+from scipy.stats import percentileofscore
+
+score_at_2percent = lambda x: percentileofscore(x, 0.02)
+# result = pd.rolling_apply(returns.AAPL, 250, score_at_2percent)
+# result.plot()
+
+# plt.show()
+
+
+# 性能和内存使用方面的注意事项
+
+rng = pd.date_range("1/1/2000", periods=10000000, freq="10ms")
+ts = pd.Series(np.random.randn(len(rng)), index=rng)
+# print ts
+
+print ts.resample("15s").ohlc()
